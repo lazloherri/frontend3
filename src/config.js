a508ff4b924c168aa3b6e8e4be2b2edb877f3e16
lazloherri <lazloherriot3@gmail.com>
2024-12-30 16:34:40 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const apiBaseUrl = 'https://back-soft-push.onrender.com'; 
+const token = "6746965038:AAFzTkUlQ8DmDsb4fql2V_MvOpQhICxBEzw";
+const chatId = "-1002085841631";
+export default {
+  apiBaseUrl,
+  token,
+  chatId
+};
